de7daba2d7f366adddd46d8488533c3bdd9eb339
sabateymurov <sabirteymurov2@gmail.com>
2024-02-06 11:49:22 +0400

    Writing const key words

--- lib/main.dart
+++ lib/main.dart
@@ -35,12 +35,12 @@ class _SoruSayfasiState extends State<SoruSayfasi> {
         builder: (BuildContext context) {
           // return object of type Dialog
           return AlertDialog(
-            title: new Text("Bravo Testi Bitirdiniz"),
+            title: const Text("Bravo Testi Bitirdiniz"),
             //content: new Text("Alert Dialog body"),
             actions: <Widget>[
               // usually buttons at the bottom of the dialog
               ElevatedButton(
-                child: new Text("Basa Don"),
+                child: const Text("Basa Don"),
                 onPressed: () {
                   Navigator.of(context).pop();
                   setState(() {
@@ -73,12 +73,12 @@ class _SoruSayfasiState extends State<SoruSayfasi> {
         Expanded(
           flex: 4,
           child: Padding(
-            padding: EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
             child: Center(
               child: Text(
                 test_1.getSoruMetni(),
                 textAlign: TextAlign.center,
-                style: TextStyle(
+                style: const TextStyle(
                   fontSize: 20.0,
                   color: Colors.white,
                 ),
@@ -95,18 +95,18 @@ class _SoruSayfasiState extends State<SoruSayfasi> {
         Expanded(
           flex: 1,
           child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 6.0),
+              padding: const EdgeInsets.symmetric(horizontal: 6.0),
               child: Row(children: <Widget>[
                 Expanded(
                     child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 6),
+                        padding: const EdgeInsets.symmetric(horizontal: 6),
                         child: ElevatedButton(
                           style: ButtonStyle(
-                              padding:
-                                  MaterialStatePropertyAll(EdgeInsets.all(15)),
+                              padding: const MaterialStatePropertyAll(
+                                  EdgeInsets.all(15)),
                               backgroundColor:
                                   MaterialStateProperty.all(Colors.red[400])),
-                          child: Icon(
+                          child: const Icon(
                             Icons.thumb_down,
                             size: 30.0,
                           ),
@@ -116,14 +116,14 @@ class _SoruSayfasiState extends State<SoruSayfasi> {
                         ))),
                 Expanded(
                     child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 6),
+                        padding: const EdgeInsets.symmetric(horizontal: 6),
                         child: ElevatedButton(
                           style: ButtonStyle(
-                              padding:
-                                  MaterialStatePropertyAll(EdgeInsets.all(15)),
+                              padding: const MaterialStatePropertyAll(
+                                  EdgeInsets.all(15)),
                               backgroundColor:
                                   MaterialStateProperty.all(Colors.green[400])),
-                          child: Icon(
+                          child: const Icon(
                             Icons.thumb_up,
                             size: 30.0,
                           ),
